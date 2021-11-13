04be272f9db33d1f13ef275d9b8542544a548c12
Shrey Marwaha <shrym.95@gmail.com>
2021-11-13 18:02:50 +0530

    Config files

---
+++ notebooks/Project-Papaya/includes/configs.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## Setting up configuration parameters for the overall project
+
+# COMMAND ----------
+
+raw_folder = "/mnt/wtf1dl/raw"
+processed_folder = "/mnt/wtf1dl/processed"
