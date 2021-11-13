d055ff48d5387823f7b0704c9d03c904e6a0d184
Shrey Marwaha <shrym.95@gmail.com>
2021-11-13 18:03:34 +0530

    Utility_file addition

---
+++ notebooks/Project-Papaya/includes/utility_functions.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ##Utility functions for other files
+
+# COMMAND ----------
+
+#All imports
+from pyspark.sql.functions import current_timestamp, lit
+
+# COMMAND ----------
+
+#Function to add ingestion_date to dataframe
+def add_ingestion_date(input_df):
+    output_df = input_df.withColumn("ingestion_date", current_timestamp())
+    return output_df
+
+# COMMAND ----------
+
+#Function to add datasource column
+def add_data_source(input_df, data_source):
+    output_df = input_df.withColumn()
+    return output_df
