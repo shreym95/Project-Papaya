133cf2defce31274868b67f7119cb46a5ad1ffce
Shrey Marwaha <shrym.95@gmail.com>
2021-11-17 19:54:02 +0530

    Moved to correct location

---
+++ notebooks/Project-Papaya/ingestion/ingest_constructors_file.py
@@ -0,0 +1,84 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### Ingest constructors.json file
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 1 - Read the JSON file using the spark dataframe reader
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType 
+
+# COMMAND ----------
+
+constructors_schema = "constructorId INT, constructorRef STRING, name STRING, nationality STRING, url STRING"
+
+# COMMAND ----------
+
+constructors_schema = StructType([StructField("constructorId", IntegerType(), False), 
+                                StructField("constructorRef", StringType(), True), 
+                                StructField("name", StringType(), True), 
+                                StructField("nationality", StringType(), True), 
+                                StructField("url", StringType(), True)])
+
+# COMMAND ----------
+
+constructor_df = spark.read \
+.schema(constructors_schema) \
+.json("/mnt/wtf1dl/raw/constructors.json")
+
+# COMMAND ----------
+
+display(constructor_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 2 - Drop unwanted columns from the dataframe
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+# COMMAND ----------
+
+constructor_dropped_df = constructor_df.drop(col('url'))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 3 - Rename columns and add ingestion date
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp
+
+# COMMAND ----------
+
+constructor_final_df = constructor_dropped_df.withColumnRenamed("constructorId", "constructor_id") \
+                                             .withColumnRenamed("constructorRef", "constructor_ref") \
+                                             .withColumn("ingestion_date", current_timestamp())
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 4 Write output to parquet file
+
+# COMMAND ----------
+
+constructor_final_df.write.mode("overwrite").format("parquet").saveAsTable("f1_processed.constructors")
+
+# COMMAND ----------
+
+display(spark.read.parquet("/mnt/wtf1dl/processed/constructors"))
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT COUNT(*) FROM f1_processed.constructors
+
+# COMMAND ----------
+
+dbutils.notebook.exit("200")
