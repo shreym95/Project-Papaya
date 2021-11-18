66c549775d786823cb742ac99df3958be142009c
Shrey Marwaha <shrym.95@gmail.com>
2021-11-19 00:30:34 +0530

    Updated to have incremental load capability as per race_id and file_date

--- notebooks/Project-Papaya/ingestion/ingest_qualifying_file.py
+++ notebooks/Project-Papaya/ingestion/ingest_qualifying_file.py
@@ -4,6 +4,22 @@
 
 # COMMAND ----------
 
+#Adding widgets to pass data source at runtime
+dbutils.widgets.text("file_date", "")
+file_date = dbutils.widgets.get("file_date")
+dbutils.widgets.text("data_source", "")
+data_source = dbutils.widgets.get("data_source")
+
+# COMMAND ----------
+
+# MAGIC %run ../includes/configs
+
+# COMMAND ----------
+
+# MAGIC %run ../includes/common_functions
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC ##### Step 1 - Read the json files using the spark dataframe reader API
 
@@ -28,7 +44,7 @@ qualifying_schema = StructType(fields=[StructField("qualifyId", IntegerType(), F
 qualifying_df = spark.read \
 .schema(qualifying_schema) \
 .option("multiLine", True) \
-.json("/mnt/wtf1dl/raw/qualifying")
+.json(f"{raw_folder}/{file_date}/qualifying")
 
 # COMMAND ----------
 
@@ -37,15 +53,17 @@ qualifying_df = spark.read \
 
 # COMMAND ----------
 
-from pyspark.sql.functions import current_timestamp
+from pyspark.sql.functions import current_timestamp, lit
 
 # COMMAND ----------
 
-final_df = qualifying_df.withColumnRenamed("driverId", "driver_id") \
+qualifying_final_df = qualifying_df.withColumnRenamed("driverId", "driver_id") \
 .withColumnRenamed("raceId", "race_id") \
 .withColumnRenamed("qualifyingId", "qualifying_id") \
 .withColumnRenamed("constructorId", "constructor_id") \
-.withColumn("ingestion_date", current_timestamp())
+.withColumn("ingestion_date", current_timestamp()) \
+.withColumn("data_source", lit(data_source)) \
+.withColumn("file_date", lit(file_date))
 
 # COMMAND ----------
 
@@ -54,7 +72,7 @@ final_df = qualifying_df.withColumnRenamed("driverId", "driver_id") \
 
 # COMMAND ----------
 
-final_df.write.mode("overwrite").format("parquet").saveAsTable("f1_processed.qualifying")
+overwrite_partition(qualifying_final_df, 'f1_processed', 'qualifying', 'race_id')
 
 # COMMAND ----------
 
@@ -63,7 +81,7 @@ final_df.write.mode("overwrite").format("parquet").saveAsTable("f1_processed.qua
 
 # COMMAND ----------
 
-display(spark.read.parquet("/mnt/wtf1dl/processed/qualifying"))
+display(spark.read.parquet(f"{processed_folder}/qualifying"))
 
 # COMMAND ----------
 
